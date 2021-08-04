480560749a2b8601630e8af91b23344c8c7558a4
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-08-04 11:47:03 +0500

    txt File Reading updated

--- txtFileReading.js
+++ txtFileReading.js
@@ -4,7 +4,7 @@ fs.readFile("file.txt", "utf-8", function (err, data) {
   if (err) throw err;
   else {
     data.split(/\r?\n/).forEach((line) => {
-      console.log(line[0]);
+      console.log(line);
     });
   }
 });
