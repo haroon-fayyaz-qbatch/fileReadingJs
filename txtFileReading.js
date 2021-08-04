7644ae265a343a1bc7da42fcdfe401d6012699bf
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-08-04 11:46:46 +0500

    reading from txt file

---
+++ txtFileReading.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+fs.readFile("file.txt", "utf-8", function (err, data) {
+  if (err) throw err;
+  else {
+    data.split(/\r?\n/).forEach((line) => {
+      console.log(line[0]);
+    });
+  }
+});
